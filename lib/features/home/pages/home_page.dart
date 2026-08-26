46512081195e8ab643d23a45c19906a83fd56947
bomi03 <kbm03@sookmyung.ac.kr>
2026-08-26 22:08:18 +0900

    fix: 하단바 상태 관리

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,53 +1,20 @@
-// 테스트를 쉽게 하기 위해 임의로 만든 홈화면입니다. 이후 홈 구현시 해당 파일은 삭제해주세요.
+// 테스트를 쉽게 하기 위해 임의로 만든 홈화면입니다.
+// 이후 홈 구현 시 해당 파일은 삭제해주세요.
 
 import 'package:flutter/material.dart';
 
 import '../../../app/theme/app_colors.dart';
-import '../../../core/widgets/bottom_tab/bottom_tab_type.dart';
-import '../../../core/widgets/navigation/bottom_tab_bar.dart';
-import '../../explore/pages/explore_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  void _handleBottomTabChanged(BuildContext context, BottomTabType tab) {
-    if (tab == BottomTabType.home) {
-      return;
-    }
-
-    if (tab == BottomTabType.explore) {
-      Navigator.of(context).pushReplacement(
-        PageRouteBuilder(
-          pageBuilder: (_, __, ___) => const ExplorePage(),
-          transitionDuration: Duration.zero,
-          reverseTransitionDuration: Duration.zero,
-        ),
-      );
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.backgroundPrimary,
-      body: SafeArea(
+      body: const SafeArea(
         bottom: false,
-        child: Stack(
-          children: [
-            const Center(child: Text('홈 테스트 화면')),
-            Positioned(
-              left: 0,
-              right: 0,
-              bottom: 0,
-              child: NavigationBottomTabBar(
-                selectedTab: BottomTabType.home,
-                onTabChanged: (tab) {
-                  _handleBottomTabChanged(context, tab);
-                },
-              ),
-            ),
-          ],
-        ),
+        child: Center(child: Text('홈 테스트 화면')),
       ),
     );
   }
---
+++ lib/features/main/pages/main_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import '../../../app/theme/app_colors.dart';
+import '../../../core/widgets/bottom_tab/bottom_tab_type.dart';
+import '../../../core/widgets/navigation/bottom_tab_bar.dart';
+import '../../explore/pages/explore_page.dart';
+import '../../home/pages/home_page.dart';
+import '../../saved/pages/saved_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  BottomTabType _selectedTab = BottomTabType.home;
+
+  int get _selectedIndex {
+    switch (_selectedTab) {
+      case BottomTabType.home:
+        return 0;
+      case BottomTabType.explore:
+        return 1;
+      case BottomTabType.saved:
+        return 2;
+      case BottomTabType.my:
+        return 3;
+    }
+  }
+
+  void _handleTabChanged(BottomTabType tab) {
+    if (_selectedTab == tab) {
+      return;
+    }
+
+    setState(() {
+      _selectedTab = tab;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isKeyboardVisible = MediaQuery.viewInsetsOf(context).bottom > 0;
+
+    return Scaffold(
+      backgroundColor: AppColors.backgroundPrimary,
+      body: Stack(
+        children: [
+          IndexedStack(
+            index: _selectedIndex,
+            children: const [
+              HomePage(),
+              ExplorePage(),
+              SavedPage(),
+
+              // TODO: 마이페이지 구현 후 교체
+              SizedBox(),
+            ],
+          ),
+
+          if (!isKeyboardVisible)
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: NavigationBottomTabBar(
+                selectedTab: _selectedTab,
+                onTabChanged: _handleTabChanged,
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
