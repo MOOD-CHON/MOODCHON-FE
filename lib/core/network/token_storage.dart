ab934b2eb260395d6d302c765c4cd4aa3d5cd2dc
hyhy-j <lhjlhj10088@gmail.com>
2026-09-05 18:02:25 +0900

    feat: dio 기반 API 클라이언트 및 토큰 저장소 구축

---
+++ lib/core/network/token_storage.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class TokenStorage {
+  TokenStorage._();
+
+  static final TokenStorage instance = TokenStorage._();
+
+  static const _accessTokenKey = 'access_token';
+  static const _refreshTokenKey = 'refresh_token';
+
+  final _storage = const FlutterSecureStorage();
+
+  Future<void> saveTokens({
+    required String accessToken,
+    required String refreshToken,
+  }) async {
+    await _storage.write(key: _accessTokenKey, value: accessToken);
+    await _storage.write(key: _refreshTokenKey, value: refreshToken);
+  }
+
+  Future<String?> getAccessToken() => _storage.read(key: _accessTokenKey);
+
+  Future<String?> getRefreshToken() => _storage.read(key: _refreshTokenKey);
+
+  Future<bool> hasAccessToken() async => await getAccessToken() != null;
+
+  Future<void> clearTokens() async {
+    await _storage.delete(key: _accessTokenKey);
+    await _storage.delete(key: _refreshTokenKey);
+  }
+}
