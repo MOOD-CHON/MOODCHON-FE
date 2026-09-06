dce7224677a78e54e753be8dc146f79badf51c28
hyhy-j <lhjlhj10088@gmail.com>
2026-09-06 13:48:36 +0900

    refactor: 로그인 분기 로직을 재사용 가능한 위젯으로 분리

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,13 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 import 'app/app.dart';
+import 'core/network/api_client.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
   KakaoSdk.init(nativeAppKey: dotenv.env['KAKAO_NATIVE_APP_KEY']!);
+  ApiClient.onSessionExpired = navigateToLogin;
 
   runApp(const MoodChonApp());
 }
